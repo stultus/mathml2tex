19776358c9e448c63c41f32eaea786ff4a394943
Hrishikesh KB <hrishi.kb@gmail.com>
2019-12-13 18:20:12 +0530

    Update package structure

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='mathml2tex',
+    version='0.1',
+    packages=['mathml2tex'],
+    license='The MIT License (MIT)',
+    author='Hrishikesh Bhaskaran',
+    author_email='hrishi.kb@gmail.com',
+    url='http://stultus.in',
+    description='Convert semi html statements with MathML to semi html statements with Latex',
+    long_description=open('README.md').read(),
+    keywords='mathml latex mathml2latex ',
+    install_requires=['lxml', 'beautifulsoup4', 'htmllaundry'],
+    tests_require=['mock'],
+    test_suite='mathml2tex.test',
+)
+
